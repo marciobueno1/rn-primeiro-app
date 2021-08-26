b40200665a6b958e2c5a737e8fcd7b2c01d688d8
Marcio Bueno <marcio_bueno@apple.com>
2021-08-26 18:41:29 -0300

    Versão inicial

--- App.tsx
+++ App.tsx
@@ -1,21 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { Props } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
+export default function App(): JSX.Element {
+  console.log("teste3...");
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={{ fontSize: 49, color: "white" }}>Hello World!</Text>
+      <Text style={{ fontSize: 30, color: "yellow" }}>Novo texto!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text style={{ fontSize: 50, color: "white" }}>Hello World!</Text>
+//         <Text style={{ fontSize: 28, color: "yellow" }}>Novo texto!</Text>
+//         <StatusBar style="auto" />
+//       </View>
+//     );
+//   }
+// }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "pink",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
